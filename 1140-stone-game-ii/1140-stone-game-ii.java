0112db70b461de0ded74616a26649f7cdc7a0757
Rajan Sharma <136893055+rajansharma08@users.noreply.github.com>
2026-08-09 16:26:50 +0530

    Time: 2 ms (100%), Space: 44.4 MB (59.83%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,41 +1,46 @@
 class Solution {
+
+    private int[][] memo;
+    private int[] suffix;
     private int n;
-    private int[][][] t = new int[2][101][101]; // t[2][n+1][n+1] ~ O(n^3)
-    
-    private int solveForAlice(int[] piles, int person, int i, int M) {
-        if (i >= n) {
-            return 0;
-        }
-        
-        if (t[person][i][M] != -1) {
-            return t[person][i][M];
-        }
-        
-        int result = (person == 1) ? -1 : Integer.MAX_VALUE;
-        int stones = 0;
-        
-        for (int x = 1; x <= Math.min(2 * M, n - i); x++) {
-            stones += piles[i + x - 1];
-            
-            if (person == 1) { // Alice
-                result = Math.max(result, stones + solveForAlice(piles, 0, i + x, Math.max(M, x)));
-            } else { // Bob
-                result = Math.min(result, solveForAlice(piles, 1, i + x, Math.max(M, x)));
-            }
-        }
-        
-        return t[person][i][M] = result;
-    }
-    
+
     public int stoneGameII(int[] piles) {
+
         n = piles.length;
-        
-        for (int[][] arr2D : t) {
-            for (int[] arr1D : arr2D) {
-                Arrays.fill(arr1D, -1);
-            }
+
+        suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
         }
-        
-        return solveForAlice(piles, 1, 0, 1);
+
+        memo = new int[n][n + 1];
+
+        return dfs(0, 1);
+    }
+
+    private int dfs(int i, int M) {
+
+        if (i >= n)
+            return 0;
+
+        if (2 * M >= n - i)
+            return suffix[i];
+
+        if (memo[i][M] != 0)
+            return memo[i][M];
+
+        int best = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+
+            best = Math.max(
+                    best,
+                    suffix[i] - dfs(i + X, Math.max(M, X))
+            );
+        }
+
+        memo[i][M] = best;
+        return best;
     }
 }
